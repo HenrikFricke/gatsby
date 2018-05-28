c02d458218932655af17d0276556cce63287ec4f
Henrik Fricke <henrik.fricke@jimdo.com>
2018-05-28 20:51:25 +0200

    Init

---
+++ src/layouts/__generated__/SiteTitleQuery.ts
@@ -0,0 +1,31 @@
+
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: SiteTitleQuery
+// ====================================================
+
+export interface SiteTitleQuery_site_siteMetadata {
+  title: string | null;
+}
+
+export interface SiteTitleQuery_site {
+  siteMetadata: SiteTitleQuery_site_siteMetadata | null;
+}
+
+export interface SiteTitleQuery {
+  site: SiteTitleQuery_site | null;
+}
+
+//==============================================================
+// START Enums and Input Objects
+// All enums and input objects are included in every output file
+// for now, but this will be changed soon.
+// TODO: Link to issue to fix this.
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
